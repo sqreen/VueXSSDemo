85ffb62e801dc6b4b72e6e7e8cd97cd75a9cd636
vouill <thibaud@sqreen.io>
2017-06-06 11:43:52 +0200

    updated project

--- src/components/chat-list-xss.vue
+++ src/components/chat-list-xss.vue
@@ -5,22 +5,23 @@
       enter-active-class="animated-fast fadeInUp"
       leave-active-class="animated-fast fadeOutBottom"
     >
-    <div v-for="(message,index) in messages" :key="index">
-      <div class="card">
-  <header class="card-header">
-    <p class="card-header-title">
-      {{message.author}}
-    </p>
-  </header>
-  <div class="card-content">
-    <div class="content">
-      <div v-html="message.text"></div>
-    </div>
-  </div>
-</div>
+      <div v-for="(message,index) in messages" :key="index">
+        <div class="card">
+        <header class="card-header">
+        <p class="card-header-title">
+        {{message.author}}
+        </p>
+        </header>
+        <div class="card-content">
+          <div class="content">
+            <div v-html="message.text">
+            </div>
+          </div>
+        </div>
+      </div>
       <br />
-    </div>
-      </transition-group>
+      </div>
+    </transition-group>
   </div>
 </template>
 <script>
